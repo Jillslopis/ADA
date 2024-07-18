786b6ab5b8f733c6fe4f8d77dc425fc7f7ad7672
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-18 22:29:04 +0530

    queens.c

--- queens.c
+++ queens.c
@@ -3,14 +3,14 @@
 
 #define N 10 // Maximum value of n, change as per requirements
 
-int board[N][N]; // Board to represent rooms
+int board[N][N]; 
 
-// Function prototypes
+
 bool isSafe(int row, int col, int n);
 bool solveNQueens(int col, int n);
 void printBoard(int n);
 
-// Main function
+
 int main() {
     int n;
 
@@ -22,7 +22,7 @@ int main() {
         return 1;
     }
 
-    // Initialize the board to 0
+    
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             board[i][j] = 0;
@@ -37,19 +37,19 @@ int main() {
     return 0;
 }
 
-// Function to check if a queen can be placed safely
+
 bool isSafe(int row, int col, int n) {
-    // Check this row on left side
+    
     for (int i = 0; i < col; i++)
         if (board[row][i])
             return false;
 
-    // Check upper diagonal on left side
+    
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
         if (board[i][j])
             return false;
 
-    // Check lower diagonal on left side
+    
     for (int i = row, j = col; j >= 0 && i < n; i++, j--)
         if (board[i][j])
             return false;
@@ -57,32 +57,32 @@ bool isSafe(int row, int col, int n) {
     return true;
 }
 
-// Recursive function to solve N-Queens problem
+
 bool solveNQueens(int col, int n) {
     // Base case: If all queens are placed
     if (col >= n)
         return true;
 
-    // Try placing this queen in all rows one by one
+    
     for (int i = 0; i < n; i++) {
         if (isSafe(i, col, n)) {
-            // Place queen (patient) on board[i][col]
+            
             board[i][col] = 1;
 
-            // Recur to place rest of the queens
+            
             if (solveNQueens(col + 1, n))
                 return true;
 
-            // If placing queen in board[i][col] doesn't lead to a solution, backtrack
+            
             board[i][col] = 0; // Backtrack
         }
     }
 
-    // If queen cannot be placed in any row in this column, return false
+    
     return false;
 }
 
-// Function to print the board configuration
+
 void printBoard(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
