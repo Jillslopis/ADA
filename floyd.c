f787830e11d90bd976231cf37ae3410c45684641
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-18 22:27:00 +0530

    floyd.c

--- floyd.c
+++ floyd.c
@@ -1,67 +1,60 @@
-#include <stdio.h>
-#include <limits.h>
+#include<stdio.h>
+#include<sys/resource.h>
+#include<sys/time.h>
 
-#define MAX_N 100 // Maximum number of cities
-
-// Function prototype
-void floydWarshall(int graph[MAX_N][MAX_N], int n);
-
-int main() {
-    int n;
-    int graph[MAX_N][MAX_N];
-
-    printf("Enter the number of cities (n <= %d): ", MAX_N);
-    scanf("%d", &n);
-
-    // Input the adjacency matrix
-    printf("Enter the adjacency matrix (%d x %d):\n", n, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &graph[i][j]);
-
-    floydWarshall(graph, n);
-
-    return 0;
+int min(int a, int b ,int c){
+    return (a<(b+c))? a : b+c;
 }
 
-// Function to find shortest paths between all pairs of vertices
-void floydWarshall(int graph[MAX_N][MAX_N], int n) {
-    int dist[MAX_N][MAX_N]; // Output matrix where dist[i][j] will be the shortest distance from i to j
-
-    // Initialize dist[][]
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            dist[i][j] = graph[i][j];
-
-    // Compute shortest paths
-    for (int k = 0; k < n; k++) {
-        // Pick all vertices as source one by one
-        for (int i = 0; i < n; i++) {
-            // Pick all vertices as destination for the above picked source
-            for (int j = 0; j < n; j++) {
-                // If vertex k is on the shortest path from i to j, then update dist[i][j]
-                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
+void floyd(int W[100][100], int D[100][100], int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            D[i][j]=W[i][j];
+        }
+    }
+    
+    for(int k=0; k<n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                D[i][j] = min(D[i][j],D[i][k],D[k][j]);
             }
         }
     }
+}
 
-    // Print the shortest distances between every pair of vertices
-    printf("Shortest distances between all pairs of vertices:\n");
-    printf("   ");
-    for (int i = 0; i < n; i++)
-        printf("%3d ", i);
-    printf("\n");
-    for (int i = 0; i < n; i++) {
-        printf("%3d", i);
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INT_MAX)
-                printf(" INF");
-            else
-                printf("%4d", dist[i][j]);
+int main() {
+    struct timeval tv1, tv2;
+    struct rusage r_usage;
+    
+    int W[100][100], D[100][100], n;
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d",&n);
+    
+    printf("Enter the adjacency matrix: \n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &W[i][j]);
+        }
+    }
+
+    gettimeofday(&tv1, NULL);
+    floyd(W,D,n);
+    gettimeofday(&tv2, NULL);
+    
+    printf("The distance matrix: \n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf(" %d ", D[i][j]);
         }
         printf("\n");
     }
-}
+    
+    double elapsedTime = (tv2.tv_sec - tv1.tv_sec) * 1000000.0 + (tv2.tv_usec - tv1.tv_usec);
+    printf("\nTime of Floyd's algorithm = %f microseconds\n", elapsedTime);
+    
+    getrusage(RUSAGE_SELF, &r_usage);
+    printf("Memory usage = %ld kilobytes\n", r_usage.ru_maxrss);
 
+    return 0;
+}
