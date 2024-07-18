4a771aa0aac6203fb6dfe5cc78ef77f52dd526ae
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-18 22:20:07 +0530

    prim.c

--- prim.c
+++ prim.c
@@ -1,77 +1,50 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
+#include<sys/time.h>
+#include<sys/resource.h>
 
-#define MAX_VERTICES 100 // Maximum number of vertices
+int n,c[20][20],i,j,visited[20];
 
-// Function prototypes
-void primMST(int graph[MAX_VERTICES][MAX_VERTICES], int n);
+void prim()
+{
+ int min,b,a,k,count=0,cost=0;
+ min=999;
+ visited[1]=1; 
+ while(count<n-1)
+ {
+  min=999;
+  for(i=1;i<=n;i++)
+    for(j=1;j<=n;j++)
+      if(visited[i] && !visited[j] && min>c[i][j]) 
+      {
+       min=c[i][j]; 
+       a=i;
+       b=j;
+      }
+   printf("%d--->%d = %d\n",a,b,c[a][b]);
+   cost+=c[a][b];
+   visited[b]=1;
+   count++;
+  } 
+  printf("Total cost of Spanning tree is %d\n",cost); 
+ }
 
-int main() {
-    int n;
-    int graph[MAX_VERTICES][MAX_VERTICES];
-
-    printf("Enter the number of vertices (n <= %d): ", MAX_VERTICES);
-    scanf("%d", &n);
-
-    // Input the adjacency matrix
-    printf("Enter the adjacency matrix (%d x %d):\n", n, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &graph[i][j]);
-
-    primMST(graph, n);
-
-    return 0;
-}
-
-// Function to find MST using Prim's algorithm
-void primMST(int graph[MAX_VERTICES][MAX_VERTICES], int n) {
-    int parent[MAX_VERTICES]; // Array to store constructed MST
-    int key[MAX_VERTICES];    // Key values used to pick minimum weight edge
-
-    bool mstSet[MAX_VERTICES]; // To represent set of vertices not yet included in MST
-
-    // Initialize all keys as INFINITE
-    for (int i = 0; i < n; i++) {
-        key[i] = INT_MAX;
-        mstSet[i] = false;
-    }
-
-    // Always include first vertex in MST
-    key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
-    parent[0] = -1; // First node is always root of MST
-
-    // The MST will have n vertices
-    for (int count = 0; count < n - 1; count++) {
-        // Pick the minimum key vertex from the set of vertices not yet included in MST
-        int u = -1;
-        for (int v = 0; v < n; v++) {
-            if (!mstSet[v] && (u == -1 || key[v] < key[u]))
-                u = v;
-        }
-
-        // Add the picked vertex to the MST set
-        mstSet[u] = true;
-
-        // Update key and parent index of the adjacent vertices of the picked vertex.
-        // Consider only those vertices which are not yet included in MST
-        for (int v = 0; v < n; v++) {
-            // graph[u][v] is non zero only for adjacent vertices of m
-            // mstSet[v] is false for vertices not yet included in MST
-            // Update the key only if graph[u][v] is smaller than key[v]
-            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
-            }
-        }
-    }
-
-    // Print the constructed MST
-    printf("Edges of Minimum Spanning Tree:\n");
-    printf("Edge\tWeight\n");
-    for (int i = 1; i < n; i++) {
-        printf("%d - %d\t%d\n", parent[i], i, graph[i][parent[i]]);
-    }
-}
-
+void main()
+{
+    struct timeval tv1, tv2;
+    struct rusage r_usage;
+printf("Enter number of vertices \n");
+ scanf("%d",&n);
+ printf("Enter the cost matrix \n");
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+	scanf("%d",&c[i][j]);
+  visited[i]=0;
+ }
+ gettimeofday(&tv1, NULL);
+ prim();
+ gettimeofday(&tv2, NULL);
+ printf("Total time = %f microseconds\n", (double)(tv2.tv_usec - tv1.tv_usec));
+ getrusage(RUSAGE_SELF, &r_usage);
+ printf("Memory usage: %ld kilobytes\n", r_usage.ru_maxrss);
+ }
