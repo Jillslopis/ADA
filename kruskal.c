eeb96cbd43ef3fba445b327961be1dd67c50987c
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-18 22:21:46 +0530

    kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,78 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/resource.h>
 
-// Structure to represent an edge in the graph
-struct Edge {
-    int src, dest, weight;
-};
+int min, cost[100][100],parent[100],i,j,x,y,n;
 
-// Structure to represent a subset for union-find
-struct subset {
-    int parent;
-    int rank;
-};
+void find_min()
+ {
+  min=999;
+  for(i=1;i<=n;i++)
+   for(j=1;j<=n;j++)
+     if((cost[i][j])<min)
+     {
+      min=cost[i][j];
+      x=i;
+      y=j;
+     }
+   }
 
-// Find function with path compression
-int find(struct subset subsets[], int i) {
-    if (subsets[i].parent != i)
-        subsets[i].parent = find(subsets, subsets[i].parent);
-    return subsets[i].parent;
-}
+   int check_cycle(int x,int y)
+   {
+    if((parent[x]==parent[y]) && (parent[x]!=0))
+	return 0;
+    else if (parent[x]==0 && parent[y]==0)
+	parent[x]=parent[y]=x;
+    else if(parent[x]==0)
+	parent[x]=parent[y];
+    else if(parent[x]!=parent[y])
+	parent[y]=parent[x];
+    return 1;
+   }
 
-// Union function by rank
-void Union(struct subset subsets[], int x, int y) {
-    int xroot = find(subsets, x);
-    int yroot = find(subsets, y);
-    if (subsets[xroot].rank < subsets[yroot].rank)
-        subsets[xroot].parent = yroot;
-    else if (subsets[xroot].rank > subsets[yroot].rank)
-        subsets[yroot].parent = xroot;
-    else {
-        subsets[yroot].parent = xroot;
-        subsets[xroot].rank++;
-    }
-}
-
-// Comparator function for qsort
-int compare(const void* a, const void* b) {
-    return ((struct Edge*)a)->weight - ((struct Edge*)b)->weight;
-}
-
-// Function to find MST using Kruskal's algorithm
-void KruskalMST(struct Edge* edges, int V, int E) {
-    struct Edge result[V];
-    struct subset subsets[V];
-    for (int v = 0; v < V; v++) {
-        subsets[v].parent = v;
-        subsets[v].rank = 0;
-    }
-    qsort(edges, E, sizeof(edges[0]), compare);
-    int e = 0, i = 0;
-    while (e < V - 1 && i < E) {
-        struct Edge next_edge = edges[i++];
-        int x = find(subsets, next_edge.src);
-        int y = find(subsets, next_edge.dest);
-        if (x != y) {
-            result[e++] = next_edge;
-            Union(subsets, x, y);
-        }
-    }
-    printf("Edges of Minimum Spanning Tree:\n");
-    for (i = 0; i < e; ++i)
-        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
-}
-
-int main() {
-    int V = 4; // Number of vertices
-    int E = 5; // Number of edges
-    struct Edge edges[] = {
-        {0, 1, 10},
-        {0, 2, 6},
-        {0, 3, 5},
-        {1, 3, 15},
-        {2, 3, 4}
-    };
-    KruskalMST(edges, V, E);
-    return 0;
-}
-
+void main()
+{
+    struct timeval tv1, tv2;
+    struct rusage r_usage;
+ int count=0,tot=0,flag=0;
+printf("Enter the number of vertices \n");
+ scanf("%d",&n);
+ printf("Enter the cost matrix \n");
+ printf("Enter 999 for No edges and self loops \n");
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+  {
+   scanf("%d",&cost[i][j]);
+   parent[j]=0;
+  }
+ }
+ gettimeofday(&tv1, NULL);
+  while(count!=n-1 && min!=999)
+  {
+   find_min();
+   flag=check_cycle(x,y);
+   if(flag==1)
+   {
+    printf("\n%d----->%d==%d\n",x,y,cost[x][y]);
+    count++;
+    tot+=cost[x][y];
+   }
+   cost[x][y]=cost[y][x]=999;
+  }
+    printf("\nThe total cost of minimum spanning tree=%d\n",tot);
+  gettimeofday(&tv2, NULL);
+  printf("Total time = %f microseconds\n", (double)(tv2.tv_usec-tv1.tv_usec));
+  getrusage(RUSAGE_SELF, &r_usage);
+  printf("Memory usage: %ld kilobytes\n", r_usage.ru_maxrss);
+  }
