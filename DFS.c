fead8a35ccb5168422ece233f0dee280ffcdd5e6
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-19 18:19:15 +0530

    DFS.c

--- DFS.c
+++ DFS.c
@@ -1,44 +1,53 @@
 #include<stdio.h>
 #include<sys/time.h>
-#include<time.h>
 #include<sys/resource.h>
-int n,a[20][20],reach[20];
+#include<time.h>
 
-void dfs(int i);
-
-void main(){
-struct timeval tv1,tv2;
-struct rusage r_usage;
-
-int i, j;
-int v;
-printf("Enter the number of vertex: ");
-scanf("%d",&n);
-
-printf("Enter the matrix:\n");
-for(i=1;i<=n;++i){
-for(j=1;j<=n;++j){
-scanf("%d",&a[i][j]);
-}
+int a[20][20], reach[20], n;
+void dfs(int v) {
+    int i;
+    reach[v] = 1;
+    for (i = 1; i <= n; i++)
+        if (a[v][i] && !reach[i]) {
+            printf("\n %d->%d", v, i);
+            dfs(i);
+        }
 }
 
-gettimeofday(&tv1,NULL);
-printf("Nodes that are reachable are:-\n");
-dfs(1);
-gettimeofday(&tv2,NULL);
+int main(int argc, char **argv) {
+    struct timeval tv1,tv2;
+    struct rusage r_usage;
+    int i, j, count = 0;
+    printf("\n Enter number of vertices:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        reach[i] = 0;
+        for (j = 1; j <= n; j++)
+            a[i][j] = 0;
+    }
+    printf("\n Enter the adjacency matrix:\n");
+    for (i = 1; i <= n; i++){
+        for (j = 1; j <= n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
 
-printf("\nTime of DFS= %f microseconds\n",(double)(tv2.tv_usec-tv1.tv_usec));
-getrusage(RUSAGE_SELF,&r_usage);
-printf("Memory usage = %ld kilobyts\n",r_usage.ru_maxrss);
+    gettimeofday(&tv1,NULL);
+    dfs(1);
+    gettimeofday(&tv2,NULL);    
+    printf("\n");
+    for (i = 1; i <= n; i++) {
+        if (reach[i])
+            count++;
+    }
+    if (count == n)
+        printf("\n Graph is connected");
+    else
+        printf("\n Graph is not connected");
+    
+    printf("\n");
+    printf("Time of selection sort= %f microsecond\n",(double)(tv2.tv_usec-tv1.tv_usec));
+    getrusage(RUSAGE_SELF,&r_usage);
+    printf("memory: %ld kilobytes\n", r_usage.ru_maxrss);
+    return 0;
 }
-
-void dfs(int i){
-printf("\n%d",i);
-reach[i]=1;
-for(int  j=1;j<=n;++j){
-    if(a[i][j]==1&&!reach[j]){
-    dfs(j);
-}
-}
-}
-
