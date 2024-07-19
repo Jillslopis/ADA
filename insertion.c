58487a31346b73315f398568cff23546e1313620
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-19 18:16:47 +0530

    insertion.c

--- insertion.c
+++ insertion.c
@@ -1,57 +1,42 @@
-#include <stdio.h>
-
-// Function prototypes
-void selectionSort(int arr[], int n, int ascending);
-void swap(int *xp, int *yp);
-void printArray(int arr[], int n);
-
-int main() {
-    int n, i, order;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter %d integers:\n", n);
-    for(i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    printf("Enter sorting order (0 for ascending, 1 for descending): ");
-    scanf("%d", &order);
-
-    selectionSort(arr, n, order);
-
-    printf("Sorted array is: ");
-    printArray(arr, n);
-
-    return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/resource.h>
+#include<sys/time.h>
+#include<time.h>
+int main(){
+struct timeval tv1,tv2;
+struct rusage r_usage;
+int arr[]={};
+int n;
+printf("Enter the number of elements\n");
+scanf("%d",&n);
+printf("ENter the elements:\n");
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&arr[i]);
 }
-
-// Selection sort function
-void selectionSort(int arr[], int n, int ascending) {
-    int i, j, min_idx;
-
-    for (i = 0; i < n-1; i++) {
-        min_idx = i;
-        for (j = i+1; j < n; j++) {
-            if (ascending ? (arr[j] < arr[min_idx]) : (arr[j] > arr[min_idx]))
-                min_idx = j;
-        }
-        swap(&arr[min_idx], &arr[i]);
-    }
+gettimeofday(&tv1,NULL);
+insertionsort(arr,n);
+gettimeofday(&tv2,NULL);
+for(int i=0;i<n;i++)
+{
+    printf("%d\n",arr[i]);
 }
-
-// Function to swap two elements
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+printf("Time of insertion sort=%f microseconds",(double)(tv2.tv_usec-tv1.tv_usec));
+getrusage(RUSAGE_SELF,&r_usage);
+printf("Memory:%ld kilobytes\n",r_usage.ru_maxrss);
 }
-
-// Function to print an array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+void insertionsort(int arr[100],int n)
+{
+int temp;
+int j;
+for(int i=1;i<n;i++){
+temp=arr[i];
+j=i-1;
+while(j>=0 && arr[j]>temp){
+arr[j+1]=arr[j];
+j--;
 }
-
+arr[j+1]=temp;
+}
+}
