a2b499909e5b3475b17b1d24a05a10e6254483c1
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-19 18:16:17 +0530

    Selectionsort.c

--- Selectionsort.c
+++ Selectionsort.c
@@ -1,63 +1,49 @@
-#include<stdio.h>
-#include<sys/time.h>
+#include<stdio.h> 
+#include<stdlib.h>
 #include<sys/resource.h>
+#include<sys/types.h>
 #include<time.h>
-#define N 4
-int arr[N];
-
-
-
-int insertionsort(){
-
-int i,j;
-int temp;
-
-printf("Enter the array ");
-
-for(i = 0; i < N; i++){
-scanf("%d", &arr[i]);
-}
-
-for(i = 1; i < N; i++){
-temp = arr[i];
-j = i - 1;
-
-while(j >= 0 && arr[j] > temp){
-arr[j + 1] = arr[j];
-j--;
-}
-arr[j + 1] = temp;
-}
-
-printf("The sorted array is");
-for(i = 0; i < N; i++){
-printf("%d ", arr[i]);
-}
-return 0;
-}
-
-int main(){
-
-
+int main()
+{
 struct timeval tv1,tv2;
-
-struct rusage r1,r2;
-getrusage(RUSAGE_SELF, &r2);
-
-gettimeofday(&tv1,NULL);
-
-
-insertionsort();
-
-gettimeofday(&tv2,NULL);
-
-
-long timetaken=tv2.tv_sec-tv1.tv_sec;
-printf("time taken is = %ld ms ", timetaken);
-
-printf(" space complexity= %ld Kb",r2.ru_maxrss);
-
-
-return 0;
+struct rusage r_usage;
+int arr[]={};
+int n;
+printf("Enter the number of elements\n");
+scanf("%d",&n);
+printf("ENter the elements:\n");
+for(int i=0;i<n;i++ )
+{
+scanf("%d",&arr[i]);
 }
-
+gettimeofday(&tv1,NULL);
+selectionsort(arr,n);
+gettimeofday(&tv2,NULL);
+printf("Sorted array\n");
+for(int i=0;i<n;i++)
+{
+printf("%d\n",arr[i]);
+}
+printf("Time of selection sort=%f microseconds",(double)(tv2.tv_usec-tv1.tv_usec));
+getrusage(RUSAGE_SELF,&r_usage);
+printf("Memory:%ld kilobytes\n",r_usage.ru_maxrss);
+}
+void selectionsort(int arr[100],int n)
+{
+int min,temp;
+int j;
+for(int i=0;i<=n-2;i++)
+{
+min=i;
+for(j=i+1;j<=n-1;j++)
+{
+if(arr[j]<arr[min]) 
+{
+min=j;
+}
+}
+temp=arr[min];
+arr[min]=arr[i];
+arr[i]=temp;
+}
+}
