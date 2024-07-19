7049a9543992acd6ca5842ca418e2964f260ad9f
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-19 18:23:02 +0530

    queens.c

--- queens.c
+++ queens.c
@@ -1,93 +1,88 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<time.h>
+#include<sys/time.h>
+#include<sys/resource.h>
+#define MAX_N 100
 
-#define N 10 // Maximum value of n, change as per requirements
-
-int board[N][N]; 
-
-
-bool isSafe(int row, int col, int n);
-bool solveNQueens(int col, int n);
-void printBoard(int n);
-
-
-int main() {
-    int n;
-
-    printf("Enter the number of patients/rooms (n <= %d): ", N);
-    scanf("%d", &n);
-
-    if (n <= 0 || n > N) {
-        printf("Invalid input. Please enter a valid value of n.\n");
-        return 1;
+void printBoard(int board[][MAX_N], int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+                printf("%c ", board[i][j] ? 'Q' : '.');
+        }
+        printf("\n");
     }
-
-    
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            board[i][j] = 0;
-
-    if (solveNQueens(0, n)) {
-        printf("Patient allocation to rooms (using N-Queens method):\n");
-        printBoard(n);
-    } else {
-        printf("Solution does not exist.\n");
-    }
-
-    return 0;
+    printf("\n");
 }
 
-
-bool isSafe(int row, int col, int n) {
-    
-    for (int i = 0; i < col; i++)
-        if (board[row][i])
+bool isSafe(int board[][MAX_N], int row, int col, int N) {
+    for (int c = 0; c < col; c++) {
+        if (board[row][c])
             return false;
+    }
 
-    
-    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
-        if (board[i][j])
+    for (int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
+        if (board[r][c])
             return false;
+    }
 
-    
-    for (int i = row, j = col; j >= 0 && i < n; i++, j--)
-        if (board[i][j])
+    for (int r = row, c = col; r < N && c >= 0; r++, c--) {
+        if (board[r][c])
             return false;
+    }
 
     return true;
 }
 
-
-bool solveNQueens(int col, int n) {
-    // Base case: If all queens are placed
-    if (col >= n)
+bool solveNQueens(int board[][MAX_N], int col, int N) {
+    if (col >= N) {
+        printBoard(board, N);
         return true;
+    }
 
-    
-    for (int i = 0; i < n; i++) {
-        if (isSafe(i, col, n)) {
-            
+    bool res = false;
+    for (int i = 0; i < N; i++) {
+        if (isSafe(board, i, col, N)) {
             board[i][col] = 1;
 
-            
-            if (solveNQueens(col + 1, n))
-                return true;
+            res = solveNQueens(board, col + 1, N) || res;
 
-            
-            board[i][col] = 0; // Backtrack
+            board[i][col] = 0;
         }
     }
 
-    
-    return false;
+    return res;
 }
 
+int main() {
+    int N;
+    struct timeval tv1, tv2;
+    struct rusage r_usage;
 
-void printBoard(int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            printf("%d ", board[i][j]);
-        printf("\n");
+    printf("Enter the size of the chessboard (N x N): ");
+    scanf("%d", &N);
+
+    if (N <= 0 || N > MAX_N) {
+        printf("Invalid size of the chessboard. Please enter a size between 1 and %d.\n", MAX_N);
+        return 1;
     }
-}
 
+    int board[MAX_N][MAX_N];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            board[i][j] = 0;
+        }
+    }
+    gettimeofday(&tv1, NULL);
+    if (!solveNQueens(board, 0, N)) {
+        printf("Solution does not exist for the given size of the chessboard.\n");
+    }
+    gettimeofday(&tv2, NULL);
+    printf("Total time = %f microseconds\n",(double)(tv2.tv_usec - tv1.tv_usec));
+    getrusage(RUSAGE_SELF,&r_usage);
+    printf("Memory usage: %ld kiloytes\n",r_usage.ru_maxrss);
+
+    return 0;
+}
