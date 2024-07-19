2b45fd895df1ef1cdb6b0689a109232e9bd21cdc
Jillslopis <153723329+Jillslopis@users.noreply.github.com>
2024-07-19 18:17:27 +0530

    quick.c

--- quick.c
+++ quick.c
@@ -1,78 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/resource.h>
-#include <time.h>
-#include <sys/types.h>
-
-// Function prototypes
-int position(int arr[], int lb, int ub);
-int quicksort(int arr[], int lb, int ub);
-
-int main() {
-    int n;
-    int lb = 0;
-    struct timeval tv1, tv2;
-    struct rusage r_usage;
-
-    printf("Enter the number of elements in an array: ");
-    scanf("%d", &n);
-
-    int *arr = (int *)malloc(n * sizeof(int)); // Allocate memory for the array
-
-    printf("Enter the elements:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    gettimeofday(&tv1, NULL);
-    quicksort(arr, lb, n - 1);
-    gettimeofday(&tv2, NULL);
-
-    printf("Sorted array:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", arr[i]);
-    }
-
-    printf("Time of quicksort = %f microseconds\n", (double)(tv2.tv_usec - tv1.tv_usec));
-    getrusage(RUSAGE_SELF, &r_usage);
-    printf("Memory: %ld kilobytes\n", r_usage.ru_maxrss);
-
-    free(arr); // Free dynamically allocated memory
-
-    return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/resource.h>
+#include<time.h>
+#include<sys/types.h>
+void main(){
+int arr[]={};
+int n;
+int lb=0;
+struct timeval tv1,tv2;
+struct rusage r_usage;
+printf("Enter the number of elements in an array\n");
+scanf("%d",&n);
+int ub=n-1;
+printf("Enter the elements:\n");
+for(int i=0;i<n;i++){
+scanf("%d",&arr[i]);
+}
+gettimeofday(&tv1,NULL);
+quicksort(arr,lb,ub);
+gettimeofday(&tv2,NULL);
+printf("Sorted array is:\n");
+for(int i=0;i<n;i++){
+printf("%d\n",arr[i]);
+}
+printf("Time of insertion sort=%f microseconds",(double)(tv2.tv_usec-tv1.tv_usec));
+getrusage(RUSAGE_SELF,&r_usage);
+printf("Memory:%ld kilobytes\n",r_usage.ru_maxrss);
 }
 
-int position(int arr[], int lb, int ub) {
-    int pivot = arr[lb];
-    int start = lb;
-    int end = ub;
-    int temp;
-
-    while (start < end) {
-        while (arr[start] <= pivot) {
-            start++;
-        }
-        while (arr[end] > pivot) {
-            end--;
-        }
-        if (start < end) {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-        }
-    }
-    temp = arr[lb];
-    arr[lb] = arr[end];
-    arr[end] = temp;
-    return end;
+int position(int arr[100],int lb,int ub){
+int pivot=arr[lb];
+int start=lb;
+int end=ub;
+int temp;
+while(start<end){
+while(arr[start]<=pivot){
+start++;
+}
+while(arr[end]>pivot){
+end--;
+}
+if(start<end){
+temp=arr[start];
+arr[start]=arr[end];
+arr[end]=temp;
+}
+}
+temp=arr[lb];
+arr[lb]=arr[end];
+arr[end]=temp;
+return end;
 }
 
-int quicksort(int arr[], int lb, int ub) {
-    int loc;
-    if (lb < ub) {
-        loc = position(arr, lb, ub);
-        quicksort(arr, lb, loc - 1);
-        quicksort(arr, loc + 1, ub);
-    }
-    return 0;
+int quicksort(int arr[100],int lb,int ub){
+int loc;
+if(lb<ub){
+loc=position(arr,lb,ub);
+quicksort(arr,lb,loc-1);
+quicksort(arr,loc+1,ub);
+
+}
+return 0;
 }
